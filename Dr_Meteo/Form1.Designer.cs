aa42cfe95a7a2a9ec1a3bdf61ec37ee05064f97f
Administrateur <Administrateur@UTILISA-E0FOG4C>
2026-01-28 17:04:31 +0100

    Ajoutez des fichiers projet.

---
+++ Dr_Meteo/Form1.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace Dr_Meteo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Panel_Accueil = new Panel();
+            SuspendLayout();
+            // 
+            // Panel_Accueil
+            // 
+            Panel_Accueil.Location = new Point(2, 1);
+            Panel_Accueil.Name = "Panel_Accueil";
+            Panel_Accueil.Size = new Size(859, 407);
+            Panel_Accueil.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(868, 450);
+            Controls.Add(Panel_Accueil);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel Panel_Accueil;
+    }
+}
---
+++ Dr_Meteo/Form1.cs
@@ -0,0 +1,10 @@
+namespace Dr_Meteo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
